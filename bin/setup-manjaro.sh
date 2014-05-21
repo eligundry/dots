aad47ecbb5d7056c5f59eeb19c6f2e6e1922d019
Eli Gundry <eligundry@gmail.com>
2014-05-20 20:47:51 -0400

    Added vim-vagrant

--- bin/setup-manjaro.sh
+++ bin/setup-manjaro.sh
@@ -3,10 +3,12 @@
 nc='--noconfirm'
 
 # Remove crappy apps I never use and their dependencies
-yaourt -R libreoffice hexchat thunderbird xnoise mdm mdm-themes
-yaourt -Qdt $nc
+yaourt -R libreoffice hexchat thunderbird xnoise xfburn mdm mdm-themes \
+	bluez gnome-bluetooth
+yaourt -Qdt
 
 # Update the system (twice for system updates)
+yaourt -Syy
 yaourt -Syua $nc
 yaourt -Syua $nc
 
@@ -14,10 +16,11 @@ yaourt -Syua $nc
 yaourt -S vim tmux zsh terminator synapse clementine qbittorrent \
 	python2-virtualenv python-virtualenv pianobar git subversion openssh mosh \
 	gnome-disk-utility python-pip python2-pip ipython ipython2 vagrant slim \
-	slim-themes
+	slim-themes xfce4-dockbarx-plugin ruby nvidiabl nodejs php
 
-yaourt -Sa $nc xflux google-chrome dropbox spotify xfce4-dockbarx-plugin \
-	ttf-ms-fonts otf-powerline-symbols-git intel-xdk popcorntime-bin
+yaourt -Sa $nc xflux google-chrome dropbox spotify ttf-ms-fonts \
+	otf-powerline-symbols-git intel-xdk popcorntime-bin htop-solarized-vi \
+	google-talkplugin php-composer blueman-bzr
 
 yaourt -Sa ttf-google-fonts-git gvim-python
 
