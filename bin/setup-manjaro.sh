4901afaaf20d9a1d205f57c16bff41f24daa3fdf
Eli Gundry <eligundry@gmail.com>
2014-06-09 01:44:16 -0400

    Updated setup-manjaro script

--- bin/setup-manjaro.sh
+++ bin/setup-manjaro.sh
@@ -2,13 +2,14 @@
 
 nc='--noconfirm'
 
+yaourt -Syy
+
 # Remove crappy apps I never use and their dependencies
 yaourt -R libreoffice hexchat thunderbird xnoise xfburn mdm mdm-themes \
 	bluez gnome-bluetooth gedit xfce4-notes-plugin catfish
 yaourt -Qdt
 
 # Update the system (twice for system updates)
-yaourt -Syy
 yaourt -Syua
 yaourt -Syua $nc
 
@@ -16,8 +17,8 @@ yaourt -Syua $nc
 yaourt -S gvim-python3 tmux zsh terminator synapse clementine transmission-gtk \
 	gnome-disk-utility pianobar git subversion openssh mosh numix-manjaro-themes \
 	python-virtualenv python-pip ipython python-pygments vagrant slim \
-    slim-themes virtualbox xfce4-dockbarx-plugin ruby nvidiabl nodejs php \
-    weechat whois ttf-symbola multitail redshift googlecl archey3 roxterm
+	slim-themes virtualbox xfce4-dockbarx-plugin ruby nodejs php weechat \
+	whois ttf-symbola multitail redshift googlecl archey3 roxterm
 
 yaourt -Sa $nc google-chrome dropbox spotify ttf-ms-fonts caffeine-bzr \
 	otf-powerline-symbols-git intel-xdk popcorntime-bin htop-solarized-vi \
@@ -37,11 +38,6 @@ cd ~
 git clone https://github.com/eligundry/dots
 cd dots
 ./dots.sh -i
-
-# Install RVM with Ruby
-source $HOME/.profile
-cd $HOME
-curl -sSL https://get.rvm.io | bash -s stable --ruby
 
 # Setup redshift with systemd
 systemctl --user enable redshift.service
@@ -75,3 +71,8 @@ xfconf-query -c xfwm4 -p /general/workspace_names -t string -t string -t string 
 xfconf-query -c xfwm4 -p /general/wrap_windows -s "false"
 xfconf-query -c xsettings -p /Gtk/FontName -s "Roboto Light 8"
 xfconf-query -c xsettings -p /Net/ThemeName -s "Numix-Manjaro"
+
+# Install RVM with Ruby
+source $HOME/.profile
+cd $HOME
+curl -sSL https://get.rvm.io | bash -s stable --ruby
