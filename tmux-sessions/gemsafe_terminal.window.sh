219f3c8974d0f8a5cc38e217798d4f477181fa77
Eli Gundry <eligundry@gmail.com>
2015-10-15 11:38:48 -0400

    Adds completer.hist to gitignore

--- tmux-sessions/gemsafe_terminal.window.sh
+++ tmux-sessions/gemsafe_terminal.window.sh
@@ -6,7 +6,7 @@ split_v 50
 
 run_cmd "source .env/bin/activate"
 run_cmd "source .env/bin/activate && vagrant up && vagrant ssh" 1
-run_cmd "source .env/bin/activate && sleep 10 && vagrant ssh -c 'cd gemsafe && fab server'; notify-send 'Flask server crashed' --icon=dialog-error" 2
+send_keys "source .env/bin/activate && vagrant ssh -c 'cd gemsafe && fab server'; notify-send 'Flask server crashed' --icon=dialog-error" 2
 run_cmd "source .env/bin/activate && gulp" 3
 
 select_pane 0
--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -10,6 +10,7 @@ alias so='source'
 alias svn_remove='find . -name .svn -print0 | xargs -0 rm -rf'
 alias v='vim'
 alias git_clean='find . -name "*.orig" -delete'
+alias pip_upgrade_all='pip freeze --local | grep -v "^\-e" | cut -d = -f 1  | xargs -n1 pip install -U'
 
 # Debian's zsh plugin sets ag, which is silver searcher
 unalias ag
