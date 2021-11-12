dcc0d68c0d3c4c9564d65b10f552a49f2ab5f6cd
Eli Gundry <eligundry@gmail.com>
2021-11-12 12:07:59 -0500

    Switch to fnm for node stuff

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -4,3 +4,7 @@ pyclean() {
     find ${ZSH_PYCLEAN_PLACES} -type d -name "__pycache__" -delete
     find ${ZSH_PYCLEAN_PLACES} -type d -name '.ropeproject' -exec rm -rf "{}" \;
 }
+
+get_secret_from_secrets_manager() {
+    aws secretsmanager get-secret-value --secret-id "$1" | jq .SecretString | jq fromjson
+}
