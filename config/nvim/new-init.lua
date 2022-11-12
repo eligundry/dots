8449efa749e61fd892414e4278bc4222ec2892d3
Eli Gundry <eligundry@gmail.com>
2022-11-12 04:06:45 -0500

    Code completion sorta works now
    Need to get jump to definition working

--- config/nvim/new-init.lua
+++ config/nvim/new-init.lua
@@ -1,6 +1,97 @@
 -- Packer {{{
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath 'data' .. '/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system { 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path }
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
+end
+
+local packer_bootstrap = ensure_packer()
+
 require('packer').startup(function ()
   use 'wbthomason/packer.nvim'
+
+  -- LSP & Autocompletion
+  use {
+    'neovim/nvim-lspconfig',
+    requires = {
+      {
+        'williamboman/mason.nvim',
+        config = function ()
+          require("mason").setup()
+        end,
+      },
+      {
+        'williamboman/mason-lspconfig.nvim',
+        config = function ()
+          require('mason-lspconfig').setup({
+            automatic_install = true,
+            ensure_installed = {
+              'astro',
+              'bashls',
+              'cssls',
+              'dockerls',
+              'eslint',
+              'gopls',
+              'html',
+              'jsonls',
+              'pyright',
+              'sqls',
+              'sumneko_lua',
+              'tailwindcss',
+              'tsserver',
+              'vimls',
+              'yamlls',
+            },
+          })
+        end
+      },
+      {
+        'hrsh7th/nvim-cmp',
+        config = function ()
+          vim.opt.completeopt = 'menu,preview,noselect'
+          local cmp = require'cmp'
+          cmp.setup({
+            snippet = {
+              expand = function(args)
+                require('luasnip').lsp_expand(args.body)
+              end,
+            },
+            mapping = cmp.mapping.preset.insert({
+              ['<C-n>'] = cmp.mapping(cmp.mapping.select_next_item(), {'i','c'}),
+              ['<C-p>'] = cmp.mapping(cmp.mapping.select_prev_item(), {'i','c'}),
+            }),
+            sources = cmp.config.sources({
+              { name = 'nvim_lsp' },
+              { name = 'luasnip' },
+            })
+          })
+        end,
+      },
+      { 'hrsh7th/cmp-nvim-lsp' },
+      { 'saadparwaiz1/cmp_luasnip' },
+      { 'L3MON4D3/LuaSnip' },
+      {
+        'jose-elias-alvarez/null-ls.nvim',
+        config = function ()
+          local null_ls = require("null-ls")
+
+          null_ls.setup({
+            sources = {
+              null_ls.builtins.formatting.stylua,
+              null_ls.builtins.diagnostics.eslint,
+              null_ls.builtins.completion.spell,
+            },
+          })
+        end
+      }
+    }
+  }
+  --
 
   -- Treesitter (syntax highlighting)
   use {
@@ -9,34 +100,131 @@ require('packer').startup(function ()
       local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
       ts_update()
     end,
+    config = function ()
+      require'nvim-treesitter.configs'.setup {
+        ensure_installed = 'all',
+      }
+    end,
   }
 
   -- Telescope (searching)
   use {
     'nvim-telescope/telescope.nvim',
     tag = '0.1.0',
-    requires = { { 'nvim-lua/plenary.nvim' } }
+    requires = { { 'nvim-lua/plenary.nvim' } },
+    config = function ()
+      local builtin = require('telescope.builtin')
+      vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+      vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+      vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+      vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+      vim.keymap.set('n', '<leader>ft', builtin.treesitter, {})
+    end,
   }
 
-  -- File trees
+  -- File tree
   use {
     'nvim-tree/nvim-tree.lua',
     requires = {
       'nvim-tree/nvim-web-devicons',
     },
     tag = 'nightly',
+    config = function ()
+      require("nvim-tree").setup({
+        view = {
+          mappings = {
+            list = {
+              { key = 'u', action = 'dir_up' },
+              { key = 's', action = 'split' },
+              { key = 'v', action = 'vsplit' },
+              { key = 't', action = 'tabnew' },
+              { key = 'x', action = 'close_node' },
+            }
+          }
+        }
+      })
+
+      vim.keymap.set('n', '<Leader>nt', ':NvimTreeToggle<CR>')
+    end,
   }
 
   -- GUI Improvements
-  use { 'lewis6991/gitsigns.nvim', tag = 'release' }
-  use 'norcalli/nvim-base16.lua'
+  use {
+    'lewis6991/gitsigns.nvim',
+    tag = 'release',
+    config = function ()
+      require('gitsigns').setup()
+    end,
+  }
+  use {
+    'norcalli/nvim-base16.lua',
+    config = function ()
+      local base16 = require 'base16'
+      base16(base16.themes['default-dark'], true)
+
+      -- Don't make my terminal less transparent
+      vim.api.nvim_set_hl(0, 'Normal', {})
+    end,
+  }
   use 'luochen1990/rainbow'
-  use 'mbbill/undotree'
+  use {
+    'mbbill/undotree',
+    config = function ()
+      vim.keymap.set('n', '<Leader>ut', ':UndotreeShow<CR>')
+    end,
+  }
   use 'mhinz/vim-startify'
   use 'vim-airline/vim-airline'
   -- This is pinned because the new versions break base16_google_light, even
   -- though I'm not even setting them
-  use { 'vim-airline/vim-airline-themes', commit = '27e7dc5bf186c1d0977a594b398847fcc84f7e24' }
+  use {
+    'vim-airline/vim-airline-themes',
+    commit = '27e7dc5bf186c1d0977a594b398847fcc84f7e24',
+    config = function ()
+      vim.g.airline_powerline_fonts = 1
+
+      -- CSV Stuff
+      -- vim.g.airline#extensions#csv#enabled = 1
+
+      -- Version Contols Stuff
+      -- vim.g.airline#extensions#hunks#enabled = 1
+
+      -- Virtualenv
+      -- vim.g.airline#extensions#virtualenv#enabled = 1
+
+      -- Coc
+      vim.g['airline#extensions#coc#enabled'] = 1
+
+      -- Fancy Tabline
+      vim.g['airline#extensions#tabline#enabled'] = 1
+      vim.g['airline#extensions#tabline#left_sep'] = ''
+      vim.g['airline#extensions#tabline#left_alt_sep'] = ''
+      vim.g['airline#extensions#tabline#right_sep'] = ''
+      vim.g['airline#extensions#tabline#right_alt_sep'] = ''
+
+      -- Fancy Powerline symbols
+      vim.g.airline_left_sep = ''
+      vim.g.airline_left_alt_sep = ''
+      vim.g.airline_right_sep = ''
+      vim.g.airline_right_alt_sep = ''
+      vim.g['airline_symbols.branch'] = ''
+      vim.g['airline_symbols.readonly'] = ''
+      vim.g['airline_symbols.linenr'] = ''
+      -- }}}
+
+      -- Tmuxline {{{
+      vim.g.tmuxline_powerline_separators = 1
+      vim.g.tmuxline_preset = {
+        a = '#S',
+        win = { '#I', '#W' },
+        cwin = { '#I', '#W' },
+        x = '#(~/.tmux/scripts/now-playing.sh)',
+        y = { '%Y-%m-%d', '%R' },
+        z = '#H',
+        options = { ['status-justify'] = 'left' }
+      }
+    end,
+  }
 
   -- tmux (because I guess you can configure it from vim?)
   use 'edkolev/tmuxline.vim'
@@ -44,12 +232,30 @@ require('packer').startup(function ()
   -- Searching
   use 'bronson/vim-visual-star-search'
   use 'ctrlpvim/ctrlp.vim'
-  use 'mhinz/vim-grepper'
 
   -- Editor Improvements
   use 'editorconfig/editorconfig-vim'
-  use 'Raimondi/delimitMate'
-  use 'tomtom/tcomment_vim'
+  use {
+    'Raimondi/delimitMate',
+    config = function()
+      vim.g.delimitMateAutoClose = 1
+      vim.g.delimitMate_expand_cr = 1
+      vim.g.delimitMate_expand_space = 1
+      vim.g.delimitMate_smart_quotes = 1
+
+      vim.api.nvim_create_autocmd('FileType', {
+        pattern = { 'vim', 'html', 'xml', 'xhtml', 'javascriptreact', 'typescriptreact' },
+        command = 'let b:delimitMate_matchpairs = "(:),[:],{:},<:>"',
+      })
+    end,
+  }
+  use {
+    'tomtom/tcomment_vim',
+    config = function ()
+      vim.keymap.set('n', '<Leader>cc', ':TComment<CR>')
+      vim.keymap.set('v', '<Leader>cc', ':TCommentBlock<CR>')
+    end,
+  }
 
   -- Vim God Tim Pope
   -- https://twitter.com/EliGundry/status/874737347568574464
@@ -58,10 +264,30 @@ require('packer').startup(function ()
   use 'tpope/vim-dispatch'
   use 'tpope/vim-dotenv'
   use 'tpope/vim-eunuch'
-  use 'tpope/vim-fugitive'
+  use {
+    'tpope/vim-fugitive',
+    config = function ()
+      vim.keymap.set('n', '<Leader>gs', ':Git<CR>')
+      vim.keymap.set('n', '<Leader>gb', ':Git blame<CR>')
+      vim.keymap.set('n', '<Leader>gp', ':Git pushy<CR>')
+
+      vim.api.nvim_create_autocmd('FileType', {
+        pattern = 'gitcommit',
+        callback = function ()
+          vim.keymap.set('n', '<Leader>s', ':wq<CR>')
+        end,
+      })
+    end,
+  }
   use 'tpope/vim-repeat'
   use 'tpope/vim-surround'
-  use 'tpope/vim-tbone'
+  use {
+    'tpope/vim-tbone',
+    config = function ()
+      vim.keymap.set({'n', 'v'}, '<Leader>ty', ':Tyank<CR>')
+      vim.keymap.set({'n', 'v'}, '<Leader>tp', ':Tput<CR>')
+    end,
+  }
 
   -- Gists
   -- use 'mattn/gist-vim'
@@ -119,7 +345,7 @@ vim.opt.shiftround = true -- Round indents to multiples of shiftwidth
 vim.opt.copyindent = true
 vim.opt.smartindent = false -- Disabling this because it messes up pasting with indents
 
--- -- NeoVim History
+-- nvim history
 vim.opt.history = 10000
 
 -- I hate backups. There's no point anymore!
@@ -191,8 +417,8 @@ vim.opt.modelines = 0
 vim.opt.joinspaces = false
 
 -- Folds
-vim.opt.foldenable = false -- I hate folds...
-vim.opt.foldmethod = 'manual' -- ...but if there are folds, let me control them
+vim.opt.foldenable = false -- I hate folds…
+vim.opt.foldmethod = 'manual' -- …but if there are folds, let me control them
 
 -- I don't need Vim telling me where I can't go!
 vim.opt.virtualedit = 'all'
@@ -313,6 +539,7 @@ vim.opt.visualbell = false
 -- Use hybrid lines by setting both
 vim.opt.number = true
 vim.opt.relativenumber = true
+vim.opt.numberwidth = 2
 
 local _numberingMode = 0
 vim.keymap.set('n', '<Leader>rn', function()
@@ -334,6 +561,9 @@ end, { desc = 'Cycle through relative and number, just number, no numbers' })
 
 -- Change Tmux cursor in insert mode
 vim.cmd('let $NVIM_TUI_ENABLE_CURSOR_SHAPE=1')
+
+-- Make all comments italic
+vim.api.nvim_set_hl(0, 'Comment', { cterm = { italic = true }, italic = true })
 --}}}
 
 -- Keyboard shortcuts {{{
@@ -355,8 +585,8 @@ vim.keymap.set('n', '+', '<C-a>')
 vim.keymap.set('n', '-', '<C-x>')
 
 -- Easier line jumping
-vim.keymap.set('n', 'H', '^')
-vim.keymap.set('n', 'L', '$')
+vim.keymap.set({'n', 'v'}, 'H', '^')
+vim.keymap.set({'n', 'v'}, 'L', '$')
 
 vim.keymap.set('n', '<Leader><Leader>', ':nohlsearch<CR>', {
   desc = 'Toggle search highlighting'
@@ -420,7 +650,7 @@ vim.keymap.set('v', '<Leader>Y', '"+y')
 vim.keymap.set({'n', 'v'}, '<Tab>', '%', { remap = true })
 
 -- Faster and more satisfying command mode access
-vim.keymap.set('n', '<Space>', ':')
+vim.keymap.set({'n', 'v'}, '<Space>', ':')
 
 -- Toggle line wrapping
 vim.keymap.set('n', '<Leader>wp', ':set wrap!<CR>')
@@ -439,12 +669,12 @@ vim.keymap.set('n', 'gj', 'j')
 vim.keymap.set('n', 'gk', 'k')
 
 -- Bubble lines of text with optional repeat count in visual mode
-vim.keymap.set('n', '<S-j>', ":@='xp`[V`]'<CR>")
-vim.keymap.set('n', '<S-k>', ":@='xkP`[V`]'<CR>")
+-- vim.keymap.set('n', '<S-j>', ":@='xp`[V`]'<CR>")
+-- vim.keymap.set('n', '<S-k>', ":@='xkP`[V`]'<CR>")
 
 -- Shift blocks visually in visual mode and retain the selection
-vim.keymap.set('n', '<', ':<gv')
-vim.keymap.set('n', '>', ':>gv')
+vim.keymap.set('v', '<', '<gv')
+vim.keymap.set('v', '>', '>gv')
 
 -- Insert horizontal ellipsis in insert mode
 vim.keymap.set('i', '\\...', '…')
@@ -457,120 +687,75 @@ vim.keymap.set('n', '<Leader>hi', function()
 end)
 -- }}}
 
--- Plugins {{{
--- Base16 {{{
-local base16 = require 'base16'
-base16(base16.themes['default-dark'], true)
+-- LSP {{{
+local lspconfig = require('lspconfig')
+local masonLSP = require('mason-lspconfig')
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
--- Don't make my terminal less transparent
-vim.api.nvim_set_hl(0, 'Normal', {})
--- }}}
-
--- Airline {{{
-vim.g.airline_powerline_fonts = 1
-
--- CSV Stuff
--- vim.g.airline#extensions#csv#enabled = 1
-
--- Version Contols Stuff
--- vim.g.airline#extensions#hunks#enabled = 1
-
--- Virtualenv
--- vim.g.airline#extensions#virtualenv#enabled = 1
-
--- Coc
-vim.g['airline#extensions#coc#enabled'] = 1
-
--- Fancy Tabline
-vim.g['airline#extensions#tabline#enabled'] = 1
-vim.g['airline#extensions#tabline#left_sep'] = ''
-vim.g['airline#extensions#tabline#left_alt_sep'] = ''
-vim.g['airline#extensions#tabline#right_sep'] = ''
-vim.g['airline#extensions#tabline#right_alt_sep'] = ''
-
--- Fancy Powerline symbols
-vim.g.airline_left_sep = ''
-vim.g.airline_left_alt_sep = ''
-vim.g.airline_right_sep = ''
-vim.g.airline_right_alt_sep = ''
-vim.g['airline_symbols.branch'] = ''
-vim.g['airline_symbols.readonly'] = ''
-vim.g['airline_symbols.linenr'] = ''
--- }}}
-
--- Tmuxline {{{
-vim.g.tmuxline_powerline_separators = 1
-vim.g.tmuxline_preset = {
-  a = '#S',
-  win = { '#I', '#W' },
-  cwin = { '#I', '#W' },
-  x = '#(~/.tmux/scripts/now-playing.sh)',
-  y = { '%Y-%m-%d', '%R' },
-  z = '#H',
-  options = { ['status-justify'] = 'left' }
-}
--- }}}
-
--- Undotree {{{
-vim.keymap.set('n', '<Leader>ut', ':UndotreeShow<CR>')
---}}}
-
--- Telescope {{{
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
--- }}}
-
--- Treesitter {{{
-require'nvim-treesitter.configs'.setup {
-  ensure_installed = 'all',
-}
--- }}}
-
--- Fugitive {{{
-vim.keymap.set('n', '<Leader>gs', ':Git<CR>')
-vim.keymap.set('n', '<Leader>gb', ':Git blame<CR>')
-vim.keymap.set('n', '<Leader>gp', ':Git pushy<CR>')
-
-vim.api.nvim_create_autocmd('FileType', {
-  pattern = 'gitcommit',
-  callback = function ()
-    vim.keymap.set('n', '<Leader>s', ':wq<CR>')
-  end,
-})
--- }}}
-
--- TComment {{{
-vim.keymap.set('n', '<Leader>cc', ':TComment<CR>')
-vim.keymap.set('v', '<Leader>cc', ':TCommentBlock<CR>')
--- }}}
-
--- TBone {{{
-vim.keymap.set({'n', 'v'}, '<Leader>ty', ':Tyank<CR>')
-vim.keymap.set({'n', 'v'}, '<Leader>tp', ':Tput<CR>')
--- }}{
-
--- nvim-tree {{{
-require("nvim-tree").setup({
-  view = {
-    mappings = {
-      list = {
-        { key = 'u', action = 'dir_up' },
-        { key = 's', action = 'split' },
-        { key = 'v', action = 'vsplit' },
-        { key = 't', action = 'tabnew' },
-        { key = 'x', action = 'close_node' },
-      }
-    }
+local lsp_formatting = function(bufnr, isAsync)
+  vim.lsp.buf.format {
+    async = isAsync,
+    filter = function(client) return client.name == 'null-ls' end,
+    bufnr = bufnr,
   }
-})
+end
 
-vim.keymap.set('n', '<Leader>nt', ':NvimTreeToggle<CR>')
--- }}}
+local lspFormattingAugroup = vim.api.nvim_create_augroup('LspFormatting', {})
 
--- gitsigns {{{
-require('gitsigns').setup()
--- }}}
+local on_attach = function(client, bufnr)
+  vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+
+  if client.supports_method 'textDocument/formatting' then
+    vim.api.nvim_clear_autocmds { group = lspFormattingAugroup, buffer = bufnr }
+    vim.api.nvim_create_autocmd('BufWritePre', {
+      group = lspFormattingAugroup,
+      buffer = bufnr,
+      callback = function() lsp_formatting(bufnr, false) end,
+    })
+  end
+
+  vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help)
+  vim.keymap.set('n', '<Leader>D', vim.lsp.buf.type_definition)
+  vim.keymap.set('n', '<Leader>ca', vim.lsp.buf.code_action)
+  vim.keymap.set('n', '<Leader>d', vim.lsp.buf.definition)
+  -- vim.keymap.set('n', '<Leader>p', function() lsp_formatting(_opts.bufnr, true) end, _opts)
+  vim.keymap.set('n', '<Leader>rn', vim.lsp.buf.rename)
+  vim.keymap.set('n', 'K', vim.lsp.buf.hover)
+  vim.keymap.set('n', 'gD', vim.lsp.buf.declaration)
+  vim.keymap.set('n', 'gi', vim.lsp.buf.implementation)
+  vim.keymap.set('n', 'gr', vim.lsp.buf.references)
+end
+
+masonLSP.setup_handlers {
+  -- The first entry (without a key) will be the default handler
+  -- and will be called for each installed server that doesn't have
+  -- a dedicated handler.
+  function(server_name)
+    lspconfig[server_name].setup {
+      on_attach = on_attach,
+      capabilities = capabilities,
+    }
+  end,
+  ['tsserver'] = function()
+    lspconfig.tsserver.setup {
+      capabilities = capabilities,
+      on_attach = function(client, bufnr)
+        on_attach(client, bufnr)
+        -- keymaps.tsserver(bufopts)
+      end,
+    }
+  end,
+  ['sumneko_lua'] = function()
+    lspconfig.sumneko_lua.setup {
+      on_attach = on_attach,
+      settings = {
+        Lua = {
+          diagnostics = {
+            globals = { 'vim' },
+          },
+        },
+      },
+    }
+  end,
+}
 -- }}}
