1c6b47b2bc5d2ee9183de84db70accaeb9b47869
Eli Gundry <eligundry@gmail.com>
2023-11-27 13:20:34 -0500

    Failed attempt to use a handwriting font for comments

--- .wezterm.lua
+++ .wezterm.lua
@@ -5,26 +5,29 @@ if wezterm.config_builder then
   config = wezterm.config_builder()
 end
 
--- Fonts
-config.font_size = 12
-config.font = wezterm.font('MonaspiceNe Nerd Font Mono', { weight = 'DemiBold' })
-config.font_rules = {
-  {
-    italic = true,
-    font = wezterm.font('MonaspiceRn Nerd Font Mono', {
-      italic = true,
-      weight = 'DemiBold',
-    })
-  },
-  {
-    italic = true,
-    intensity = 'Bold',
-    font = wezterm.font('MonaspiceRn Nerd Font Mono', {
-      italic = true,
-      weight = 'Bold',
-    })
-  },
-}
+-- Fonts (I would love to be a person that could be into a handwriting font for
+-- comments, but I'm just not that person.)
+config.font_size = 13
+config.font = wezterm.font('CaskaydiaCove Nerd Font Mono', { weight = 'DemiBold' })
+-- config.font_size = 12
+-- config.font = wezterm.font('MonaspiceNe Nerd Font Mono', { weight = 'DemiBold' })
+-- config.font_rules = {
+--   {
+--     italic = true,
+--     font = wezterm.font('MonaspiceRn Nerd Font Mono', {
+--       italic = true,
+--       weight = 'DemiBold',
+--     })
+--   },
+--   {
+--     italic = true,
+--     intensity = 'Bold',
+--     font = wezterm.font('MonaspiceRn Nerd Font Mono', {
+--       italic = true,
+--       weight = 'Bold',
+--     })
+--   },
+-- }
 
 -- Transparency
 config.window_background_opacity = 0.95
