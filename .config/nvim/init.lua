ca7f1e03089cb68c495fedf12b173c1586ede7d8
Eli Gundry <eligundry@gmail.com>
2024-01-03 14:19:40 -0500

    Upgrade some neovim packages

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -207,7 +207,7 @@ require("lazy").setup(
     },
     {
       "nvim-telescope/telescope.nvim",
-      tag = "0.1.2",
+      branch = "0.1.x",
       dependencies = {
         "nvim-lua/plenary.nvim",
         {
