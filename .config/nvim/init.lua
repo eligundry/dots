1cb79d21be140e6f3a6cd87d53645e0ab87d0a97
Eli Gundry <eligundry@gmail.com>
2024-01-08 14:16:28 -0500

    Remove telescope-frecency.nvim as it's annoying

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -210,12 +210,6 @@ require("lazy").setup(
       branch = "0.1.x",
       dependencies = {
         "nvim-lua/plenary.nvim",
-        {
-          "nvim-telescope/telescope-frecency.nvim",
-          config = function()
-            require("telescope").load_extension "frecency"
-          end,
-        }
       },
       cmd = { "Telescope" },
       keys = function()
