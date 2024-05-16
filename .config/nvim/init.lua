a054de1e6b5ec20769b1bff771c3aaddfee45dfb
Eli Gundry <eligundry@gmail.com>
2024-05-16 15:52:30 -0400

    Make gp.nvim chats sync to Dropbox

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -720,9 +720,51 @@ require("lazy").setup(
     {
       "robitx/gp.nvim",
       config = true,
-      opts = {
-        openai_api_key = { "pass", "Dev/openai.com/gp.nvim-api-key" },
-      },
+      opts = function()
+        local chat_dir = vim.fn.stdpath("data"):gsub("/$", "") .. "/gp/chats"
+        local image_dir = (os.getenv("TMPDIR") or os.getenv("TEMP") or "/tmp") .. "/gp_images"
+
+        if vim.fn.isdirectory(vim.fn.expand("~/Dropbox/nvim/gp.nvim")) == 1 then
+          chat_dir = vim.fn.expand("~/Dropbox/nvim/gp.nvim/chats")
+          image_dir = vim.fn.expand("~/Dropbox/nvim/gp.nvim/images")
+        end
+
+        return {
+          openai_api_key = { "pass", "Dev/openai.com/gp.nvim-api-key" },
+          chat_dir = chat_dir,
+          image_dir = image_dir,
+          agents = {
+            {
+              name = "ChatGPT4",
+              chat = true,
+              command = false,
+              -- string with model name or table with model name and parameters
+              model = { model = "gpt-4o", temperature = 1.1, top_p = 1 },
+              -- system prompt (use this to specify the persona/role of the AI)
+              system_prompt = "You are a general AI assistant.\n\n"
+                  .. "The user provided the additional info about how they would like you to respond:\n\n"
+                  .. "- If you're unsure don't guess and say you don't know instead.\n"
+                  .. "- Ask question if you need clarification to provide better answer.\n"
+                  .. "- Think deeply and carefully from first principles step by step.\n"
+                  .. "- Zoom out first to see the big picture and then zoom in to details.\n"
+                  .. "- Use Socratic method to improve your thinking and coding skills.\n"
+                  .. "- Don't elide any code from your output if the answer requires coding.\n"
+                  .. "- Take a deep breath; You've got this!\n",
+            },
+            {
+              name = "CodeGPT4",
+              chat = true,
+              command = true,
+              -- string with model name or table with model name and parameters
+              model = { model = "gpt-4o", temperature = 0.8, top_p = 1 },
+              -- system prompt (use this to specify the persona/role of the AI)
+              system_prompt = "You are an AI working as a code editor.\n\n"
+                  .. "Please AVOID COMMENTARY OUTSIDE OF THE SNIPPET RESPONSE.\n"
+                  .. "START AND END YOUR ANSWER WITH:\n\n```",
+            },
+          },
+        }
+      end,
     },
     -- }}}
     -- Commenting {{{
