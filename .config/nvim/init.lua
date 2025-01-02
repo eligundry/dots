c2dbf7c245f5737342988a2c078725feb3e33c66
Eli Gundry <eligundry@gmail.com>
2025-01-02 17:52:23 -0500

    Configure pyenv + ghostty

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -743,39 +743,21 @@ require("lazy").setup(
 
         return {
           openai_api_key = { "pass", "Dev/openai.com/gp.nvim-api-key" },
+          providers = {
+            openai = {
+              secret = { "pass", "Dev/openai.com/gp.nvim-api-key" },
+            },
+            anthropic = {
+              secret = { "pass", "Dev/anthropic.com/api-key" }
+            },
+          },
           chat_dir = chat_dir,
           image = {
             store_dir = image_dir,
           },
           agents = {
-            {
-              name = "ChatGPT4",
-              chat = true,
-              command = false,
-              -- string with model name or table with model name and parameters
-              model = { model = "gpt-4o", temperature = 1.1, top_p = 1 },
-              -- system prompt (use this to specify the persona/role of the AI)
-              system_prompt = "You are a general AI assistant.\n\n"
-                  .. "The user provided the additional info about how they would like you to respond:\n\n"
-                  .. "- If you're unsure don't guess and say you don't know instead.\n"
-                  .. "- Ask question if you need clarification to provide better answer.\n"
-                  .. "- Think deeply and carefully from first principles step by step.\n"
-                  .. "- Zoom out first to see the big picture and then zoom in to details.\n"
-                  .. "- Use Socratic method to improve your thinking and coding skills.\n"
-                  .. "- Don't elide any code from your output if the answer requires coding.\n"
-                  .. "- Take a deep breath; You've got this!\n",
-            },
-            {
-              name = "CodeGPT4",
-              chat = true,
-              command = true,
-              -- string with model name or table with model name and parameters
-              model = { model = "gpt-4o", temperature = 0.8, top_p = 1 },
-              -- system prompt (use this to specify the persona/role of the AI)
-              system_prompt = "You are an AI working as a code editor.\n\n"
-                  .. "Please AVOID COMMENTARY OUTSIDE OF THE SNIPPET RESPONSE.\n"
-                  .. "START AND END YOUR ANSWER WITH:\n\n```",
-            },
+            { name = "ChatGPT3-5",     disable = true },
+            { name = "ChatGPT4o-mini", disable = true },
           },
         }
       end,
@@ -786,8 +768,12 @@ require("lazy").setup(
       lazy = false,
       version = false,
       opts = {
-        provider = "openai",
+        provider = "claude",
+        -- provider = "openai",
         auto_suggestions_provider = "copilot",
+        behaviour = {
+          auto_suggestions = true,
+        },
       },
       build = "make",
       dependencies = {
