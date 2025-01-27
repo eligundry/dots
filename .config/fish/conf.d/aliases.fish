d3ad45979a4f6390bccd666d451b05bbe49c8082
Eli Gundry <eligundry@gmail.com>
2025-01-27 12:23:15 -0500

    Add some more aliases

--- .config/fish/conf.d/aliases.fish
+++ .config/fish/conf.d/aliases.fish
@@ -3,11 +3,13 @@ if status is-interactive
   alias cp='cp -v'
   alias mv='mv -v'
   alias rm='rm -v'
+  alias md='mkdir -pv'
   alias s='sudo'
   alias la='ls -A'
   alias ll='ls -lh'
 
   alias g='git'
   alias gst='git status'
+  alias gco='git checkout'
   alias gcotb='git checkout --track=direct -b'
 end
