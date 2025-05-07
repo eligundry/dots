8c88dadc66ebf1bf64125f6d64727839847c0006
Eli Gundry <eligundry@gmail.com>
2025-05-07 12:26:56 -0400

    Update `git get-co-authors` to get co-authored-bys

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -50,3 +50,7 @@ if status is-interactive
   end
 end
 
+
+# Added by OrbStack: command-line tools and integration
+# This won't be added again if you remove it.
+source ~/.orbstack/shell/init2.fish 2>/dev/null || :
