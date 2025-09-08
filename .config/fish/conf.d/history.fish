77bc28a362f15d5a034da284233ec47a73daec49
Eli Gundry <eligundry@gmail.com>
2025-09-08 15:26:47 -0400

    Atuin's search is very good

--- .config/fish/conf.d/history.fish
+++ .config/fish/conf.d/history.fish
@@ -1,12 +1,2 @@
 # Sync shell history to the cloud with atuin
-set -gx ATUIN_NOBIND "true"
 atuin init fish | source
-
-# Bind ctrl-r to atuin search
-bind \cr _atuin_search
-
-# Bind up arrow to search through history with current input
-bind -k up '_atuin_search_widget --shell-up-key-binding'
-
-# Bind down arrow to search through history with current input
-bind -k down '_atuin_search_widget --shell-down-key-binding'
