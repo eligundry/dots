5dac8a1c0f4bc2cab1b654a1ebcf9cb159bb60b0
Eli Gundry <eligundry@gmail.com>
2025-10-21 11:03:02 -0400

    Bunch of lil updates
    - Add `,pwd` to copy relative paths in neovim for easier prompting
    - Update `base16` to set the Claude theme based upon the base16 theme's
      luminance
    - Upgrade neovim packages

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1477,8 +1477,11 @@ vim.keymap.set("n", "<Leader>fl", vim.diagnostic.setqflist, {
 })
 
 vim.keymap.set("n", "<Leader>pwd", function()
-  print(vim.fn.expand("%:p"))
-end, { desc = "Print path to current file" })
+  local absolute_path = vim.fn.expand("%:p")
+  local relative_path = vim.fn.fnamemodify(absolute_path, ":~:.")
+  vim.fn.setreg("+", relative_path)
+  print(relative_path .. " (copied to clipboard)")
+end, { desc = "Print relative path to current file and copy to clipboard" })
 
 -- My ideal state of using vim is to have it always in autochdir. This means,
 -- whenever I open a new a file in a different directory, all vim commands for
